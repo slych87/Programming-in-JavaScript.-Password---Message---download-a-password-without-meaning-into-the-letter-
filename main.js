1f31aba840846f1e1178f87ed605319f5d92e0a6
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-09 16:39:40 +0100

    second option

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ const input = document.querySelector("input");
 const passwords = ['jedEN', 'DwA'];
 const messages = ["super", "działa!"]
 
+passwords.forEach((password, index) => {
+    passwords[index] = password.toLowerCase();
+    console.log[index];
+})
 const showMessage = (e) => {
-    passwords.forEach((item, i) => {
-        if (item.toLowerCase() === e.target.value.toLowerCase()) {
-            document.querySelector('div').textContent = messages[i];
-        }
+    const input = e.target.value.toLowerCase();
+    passwords.forEach((password, i) => {
+        document.querySelector('div').textContent = messages[i];
     })
-
 }
 
 input.addEventListener("input", showMessage)
