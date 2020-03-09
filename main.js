18b3d160334fbe2ef20b6918f14d116d25c8a2cf
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-09 17:51:56 +0100

    map methid

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const input = document.querySelector("input");
 const passwords = ['jedEN', 'DwA'];
 const messages = ["super", "działa!"]
+const div = document.querySelector('div');
+
+[10, 45, 66].map(number => number * 3)
+
+const LCPasswords = passwords.map(password => password.toLowerCase())
 
 const showMessage = (e) => {
-    passwords.forEach((item, i) => {
-        if (item.toLowerCase() === e.target.value.toLowerCase()) {
-            document.querySelector('div').textContent = messages[i];
+    const textInput = e.target.value.toLowerCase();
+
+    for (let i = 0; i < LCPasswords.length; i++) {
+        if (textInput === LCPasswords[i]) {
+            div.innerHTML = messages[i];
         }
-    })
+    }
 
 }
 
