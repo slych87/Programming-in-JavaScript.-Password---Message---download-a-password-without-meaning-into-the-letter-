ced87b768fd5b0b63f6df95fe6e0e728e31c8c4e
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-09 16:15:25 +0100

    end

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const passwords = ['jedEN', 'DwA'];
 const messages = ["super", "działa!"]
 
 const showMessage = (e) => {
- passwords.forEach((item, i) => {
-  if (item.toLowerCase() === e.target.value.toLowerCase()) {
-   document.querySelector('div').textContent = messages[i];
-  }
- })
+    passwords.forEach((item, i) => {
+        if (item.toLowerCase() === e.target.value.toLowerCase()) {
+            document.querySelector('div').textContent = messages[i];
+        }
+    })
+
 }
 
 input.addEventListener("input", showMessage)
